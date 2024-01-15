59bbe12410a5b10c298bc87638979159d085aeb3
lucenac <joaopedrolucena568@gmail.com>
2024-01-15 18:08:28 -0300

    add Position Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,4 +1,11 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
+    public static void main(String[] args) {
+        Position pos = new Position(5, 3);
+
+        System.out.println(pos);
+    }
 }
