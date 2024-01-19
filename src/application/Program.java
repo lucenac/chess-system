cfa03ae3a7a7a657ab864d487ae44f9ebb9c4c68
lucenac <joaopedrolucena568@gmail.com>
2024-01-18 23:39:19 -0300

    adding possible moves print

--- src/application/Program.java
+++ src/application/Program.java
@@ -25,6 +25,9 @@ public class Program {
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
 
+                boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces(), possibleMoves);
                 System.out.println();
                 System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
@@ -38,7 +41,7 @@ public class Program {
             }
             catch (InputMismatchException e) {
                 System.out.println(e.getMessage());
-                System.out.println("Press Enter");
+                System.out.println("Press Enter ");
                 sc.nextLine();
             }
 
